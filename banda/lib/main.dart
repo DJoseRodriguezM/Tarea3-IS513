add488e7cfbe20eab4307c195cc9bffccd902540
David Jose Rodriguez <dj.rodriguez@unah.hn>
2024-04-26 00:31:59 -0600

    quitando el bannerdebug

--- banda/lib/main.dart
+++ banda/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: MyRoutes.home.name,
       routes: routes,
